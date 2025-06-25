0cd972ec29f7a848b1236dba191209f979fd0950
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-06-25 09:17:15 +0530

    Create index.js

---
+++ Assignment/Assignment 4/index.js
@@ -0,0 +1,67 @@
+
+const grandparent = document.getElementById('grandparent');
+const parent = document.getElementById('parent');
+const child = document.getElementById("child");
+
+
+//// EVENT CAPTURING
+
+
+
+
+
+
+grandparent.addEventListener('click', () => {
+    console.log("GRANDPARENT DIV CLICKED");
+}, true);
+
+parent.addEventListener("click", () => {
+    console.log("PARENT DIV CLICKED");
+}, true);
+
+child.addEventListener("click", () => {
+    console.log("CHILD DIV CLICKED");
+}, true);
+
+
+
+
+////  EVENT BUBBLING
+
+
+
+grandparent.addEventListener('click', (e) => {
+    if (e.target === grandparent) {
+        console.log("GRANDPARENT DIV CLICKED");
+    }
+}, true); 
+
+grandparent.addEventListener('click', (e) => {
+    if (e.target === grandparent) {
+        console.log("PARENT DIV CLICKED");
+    }
+}, true);
+grandparent.addEventListener('click', (e) => {
+    if (e.target === grandparent) {
+        console.log("CHILD DIV CLICKED");
+    }
+}, true); 
+
+
+parent.addEventListener('click', (e) => {
+    if (e.target === parent) {
+        console.log("PARENT DIV CLICKED");
+    }
+}, true); 
+
+parent.addEventListener('click', (e) => {
+    if (e.target === parent) {
+        console.log("CHILD DIV CLICKED");
+    }
+}, true); 
+
+child.addEventListener('click', (e) => {
+    if (e.target === child) {
+        console.log("CHILD DIV CLICKED");
+    }
+}, true);
