01bb79e9e07254164923370ab88d2819f5803567
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-07-25 10:21:15 +0530

    Create server.js

---
+++ Node.js/Lecture 7/server.js
@@ -0,0 +1,7 @@
+const app = require("./src/app")
+const connect = require("./src/db/db")
+const PORT = 3000
+app.listen(PORT , ()=>{
+    console.log("SERVER IS RUNING  ON PORT NO : ", PORT);
+    connect()
+})
